7c8b75e1780c7e85cb77748fbdf3cf6fa6c14294
James Brown <jbrown@uber.com>
2015-01-23 12:43:55 -0800

    bump to v0.3

--- pyrehol/__init__.py
+++ pyrehol/__init__.py
@@ -5,7 +5,7 @@ import types
 
 __name__ = 'pyrehol'
 __author__ = 'James Brown <jbrown@uber.com>'
-version_info = (0, 2)
+version_info = (0, 3)
 __version__ = '.'.join(map(str, version_info))
 
 INDENT_CHAR = '  '
